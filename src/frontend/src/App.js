d1fe5f9577861f3bc7f6d4650d57adee3c23becd
hiba <hiba.azhari@gmail.com>
2021-11-29 14:31:24 +0800

    Add state to React app

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,24 @@
-import {
-    Button,
-    Radio
-} from "antd";
+import { useState, useEffect } from 'react';
 import { getAllStudents } from "./client";
 import './App.css';
 
 function App() {
-    getAllStudents()
-        .then(res => res.json())
-        .then(console.log)
-  return "hello react";
+    const [students, setStudents] = useState([]);
+
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+            })
+
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, [])
+
+    return <p>{students.length}</p>;
 }
 
 export default App;
