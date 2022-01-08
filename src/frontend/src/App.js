38c12aa6bd1da17e32f799863518c28dd871c2bc
hiba <hiba.azhari@gmail.com>
2022-01-08 23:40:32 +0800

    Add student table

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -79,7 +79,7 @@ function App() {
             bordered
             title={() => 'Students'}
             pagination={{ pageSize: 50 }}
-            scroll={{ y:240 }}
+            scroll={{ y:1000 }}
             rowKey={(student) => student.id}
         />;
     }
